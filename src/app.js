4a571dd9d74af291afc697c0a5fbcacac7ec19f2
Cristina Mantiñán <56038975+CrisMB-82@users.noreply.github.com>
2025-03-19 22:17:58 +0000

    dominios .com

--- src/app.js
+++ src/app.js
@@ -14,8 +14,7 @@ window.onload = function() {
   for (let p = 0; p < pronoun.length;p++){
    for (let a = 0; a < adj.length;a++){
     for (let n = 0; n < noun.length;n++){
-      domain.push(pronoun[p]
-       + adj[a] + noun[n] + ".com") 
+      domain.push(pronoun[p] + adj[a] + noun[n] + ".com") 
     }
    }
   }
